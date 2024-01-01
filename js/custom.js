bae08656fa7c3b456dfb959dbe23b994da53b7f5
hoangduy1508 <hoangduy15081995@gmail.com>
2024-01-01 15:15:37 +0700

    update style

--- js/custom.js
+++ js/custom.js
@@ -32,20 +32,18 @@
 $(function () {
     "use strict";
     var wind = $(window);
-    
-    
+
     // ScrollIt
     $.scrollIt({
         upKey: 38, // key code to navigate to the next section
         downKey: 40, // key code to navigate to the previous section
-        easing: 'swing', // the easing function for animation
+        easing: "swing", // the easing function for animation
         scrollTime: 600, // how long (in ms) the animation takes
-        activeClass: 'active', // class given to the active nav element
+        activeClass: "active", // class given to the active nav element
         onPageChange: null, // function(pageIndex) that is called when page is changed
-        topOffset: -70 // offste (in px) for fixed top navigation
+        topOffset: -70, // offste (in px) for fixed top navigation
     });
-    
-    
+
     // Navbar scrolling background
     wind.on("scroll", function () {
         var bodyScroll = wind.scrollTop(),
@@ -53,272 +51,362 @@ $(function () {
             logo = $(".navbar .logo> img");
         if (bodyScroll > 100) {
             navbar.addClass("nav-scroll");
-            logo.attr('src', 'images/logo.png');
+            logo.attr("src", "images/logo.png");
         } else {
             navbar.removeClass("nav-scroll");
-            logo.attr('src', 'images/logo-light.png');
+            logo.attr("src", "images/logo-light.png");
         }
     });
-    
-    
+
     // Close navbar-collapse when a  clicked
-    $(".navbar-nav .dropdown-item a").on('click', function () {
+    $(".navbar-nav .dropdown-item a").on("click", function () {
         $(".navbar-collapse").removeClass("show");
     });
-    
-    
+
     // Sections background image from data background
     var pageSection = $(".bg-img, section");
     pageSection.each(function (indx) {
         if ($(this).attr("data-background")) {
-            $(this).css("background-image", "url(" + $(this).data("background") + ")");
+            $(this).css(
+                "background-image",
+                "url(" + $(this).data("background") + ")"
+            );
         }
     });
 
-    
     // Animations
     var contentWayPoint = function () {
         var i = 0;
-        $('.animate-box').waypoint(function (direction) {
-            if (direction === 'down' && !$(this.element).hasClass('animated')) {
-                i++;
-                $(this.element).addClass('item-animate');
-                setTimeout(function () {
-                    $('body .animate-box.item-animate').each(function (k) {
-                        var el = $(this);
-                        setTimeout(function () {
-                            var effect = el.data('animate-effect');
-                            if (effect === 'fadeIn') {
-                                el.addClass('fadeIn animated');
-                            } else if (effect === 'fadeInLeft') {
-                                el.addClass('fadeInLeft animated');
-                            } else if (effect === 'fadeInRight') {
-                                el.addClass('fadeInRight animated');
-                            } else {
-                                el.addClass('fadeInUp animated');
-                            }
-                            el.removeClass('item-animate');
-                        }, k * 200, 'easeInOutExpo');
-                    });
-                }, 100);
+        $(".animate-box").waypoint(
+            function (direction) {
+                if (direction === "down" && !$(this.element).hasClass("animated")) {
+                    i++;
+                    $(this.element).addClass("item-animate");
+                    setTimeout(function () {
+                        $("body .animate-box.item-animate").each(function (k) {
+                            var el = $(this);
+                            setTimeout(
+                                function () {
+                                    var effect = el.data("animate-effect");
+                                    if (effect === "fadeIn") {
+                                        el.addClass("fadeIn animated");
+                                    } else if (effect === "fadeInLeft") {
+                                        el.addClass("fadeInLeft animated");
+                                    } else if (effect === "fadeInRight") {
+                                        el.addClass("fadeInRight animated");
+                                    } else {
+                                        el.addClass("fadeInUp animated");
+                                    }
+                                    el.removeClass("item-animate");
+                                },
+                                k * 200,
+                                "easeInOutExpo"
+                            );
+                        });
+                    }, 100);
+                }
+            },
+            {
+                offset: "85%",
             }
-        }, {
-            offset: '85%'
-        });
+        );
     };
     $(function () {
         contentWayPoint();
     });
-    
-    
+
     // YouTubePopUp
     $("a.vid").YouTubePopUp();
-    
-    
+
     // Testimonials owlCarousel *
-    $('.testimonials .owl-carousel').owlCarousel({
+    $(".testimonials .owl-carousel").owlCarousel({
         loop: true,
         margin: 30,
         mouseDrag: true,
         autoplay: false,
         dots: true,
         nav: false,
-        navText: ["<span class='lnr ti-angle-left'></span>", "<span class='lnr ti-angle-right'></span>"],
+        navText: [
+            "<span class='lnr ti-angle-left'></span>",
+            "<span class='lnr ti-angle-right'></span>",
+        ],
         responsiveClass: true,
         responsive: {
             0: {
                 items: 1,
             },
             600: {
-                items: 1
+                items: 1,
             },
             1000: {
-                items: 1
-            }
-        }
+                items: 1,
+            },
+        },
     });
-
-    
 
     // Smooth Scrolling
     $('a[href*="#"]')
-    // Remove links that don't actually link to anything
-    .not('[href="#"]').not('[href="#0"]').click(function (event) {
-        // On-page links
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            // Figure out element to scroll to
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            // Does a scroll target exist?
-            if (target.length) {
-                // Only prevent default if animation is actually gonna happen
-                event.preventDefault();
-                $('html, body').animate({
-                    scrollTop: target.offset().top
-                }, 1000, function () {
-                    // Callback after animation
-                    // Must change focus!
-                    var $target = $(target);
-                    $target.focus();
-                    if ($target.is(":focus")) { // Checking if the target was focused
-                        return false;
-                    } else {
-                        $target.attr('tabindex', '-1'); // Adding tabindex for elements not focusable
-                        $target.focus(); // Set focus again
-                    };
-                });
+        // Remove links that don't actually link to anything
+        .not('[href="#"]')
+        .not('[href="#0"]')
+        .click(function (event) {
+            // On-page links
+            if (
+                location.pathname.replace(/^\//, "") ==
+                this.pathname.replace(/^\//, "") &&
+                location.hostname == this.hostname
+            ) {
+                // Figure out element to scroll to
+                var target = $(this.hash);
+                target = target.length
+                    ? target
+                    : $("[name=" + this.hash.slice(1) + "]");
+                // Does a scroll target exist?
+                if (target.length) {
+                    // Only prevent default if animation is actually gonna happen
+                    event.preventDefault();
+                    $("html, body").animate(
+                        {
+                            scrollTop: target.offset().top,
+                        },
+                        1000,
+                        function () {
+                            // Callback after animation
+                            // Must change focus!
+                            var $target = $(target);
+                            $target.focus();
+                            if ($target.is(":focus")) {
+                                // Checking if the target was focused
+                                return false;
+                            } else {
+                                $target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+                                $target.focus(); // Set focus again
+                            }
+                        }
+                    );
+                }
             }
-        }
-    });
-    
+        });
+
     //  Scroll back to top
-    var progressPath = document.querySelector('.progress-wrap path');
+    var progressPath = document.querySelector(".progress-wrap path");
     var pathLength = progressPath.getTotalLength();
-    progressPath.style.transition = progressPath.style.WebkitTransition = 'none';
-    progressPath.style.strokeDasharray = pathLength + ' ' + pathLength;
+    progressPath.style.transition = progressPath.style.WebkitTransition = "none";
+    progressPath.style.strokeDasharray = pathLength + " " + pathLength;
     progressPath.style.strokeDashoffset = pathLength;
     progressPath.getBoundingClientRect();
-    progressPath.style.transition = progressPath.style.WebkitTransition = 'stroke-dashoffset 10ms linear';
+    progressPath.style.transition = progressPath.style.WebkitTransition =
+        "stroke-dashoffset 10ms linear";
     var updateProgress = function () {
         var scroll = $(window).scrollTop();
         var height = $(document).height() - $(window).height();
-        var progress = pathLength - (scroll * pathLength / height);
+        var progress = pathLength - (scroll * pathLength) / height;
         progressPath.style.strokeDashoffset = progress;
-    }
+    };
     updateProgress();
     $(window).scroll(updateProgress);
     var offset = 150;
     var duration = 550;
-    jQuery(window).on('scroll', function () {
+    jQuery(window).on("scroll", function () {
         if (jQuery(this).scrollTop() > offset) {
-            jQuery('.progress-wrap').addClass('active-progress');
+            jQuery(".progress-wrap").addClass("active-progress");
         } else {
-            jQuery('.progress-wrap').removeClass('active-progress');
+            jQuery(".progress-wrap").removeClass("active-progress");
         }
     });
-    jQuery('.progress-wrap').on('click', function (event) {
+    jQuery(".progress-wrap").on("click", function (event) {
         event.preventDefault();
-        jQuery('html, body').animate({
-            scrollTop: 0
-        }, duration);
+        jQuery("html, body").animate(
+            {
+                scrollTop: 0,
+            },
+            duration
+        );
         return false;
-    })
-    
-    
+    });
+
     // Select2
-    $('.select2').select2({
+    $(".select2").select2({
         minimumResultsForSearch: Infinity,
     });
-    
-    
+
     // Datapicker
     $(".datepicker").datepicker({
-        orientation: "top"
+        orientation: "top",
     });
-     
 });
 
- 
-// Slider  
+// Slider
 $(document).ready(function () {
-    var owl = $('.header .owl-carousel');
-    
+    var owl = $(".header .owl-carousel");
+
     // Slider owlCarousel - (Inner Page Slider)
-    $('.slider .owl-carousel').owlCarousel({
+    $(".slider .owl-carousel").owlCarousel({
         items: 1,
         loop: true,
         dots: true,
         margin: 0,
-        autoplay: true,
+        autoplay: false,
         autoplayTimeout: 5000,
         nav: false,
-        navText: ['<i class="ti-angle-left" aria-hidden="true"></i>', '<i class="ti-angle-right" aria-hidden="true"></i>'],
+        navText: [
+            '<i class="ti-angle-left" aria-hidden="true"></i>',
+            '<i class="ti-angle-right" aria-hidden="true"></i>',
+        ],
         responsiveClass: true,
         responsive: {
             0: {
-                dots: true
+                dots: true,
             },
             600: {
-                dots: true
+                dots: true,
             },
             1000: {
-                dots: true
-            }
-        }
+                dots: true,
+            },
+        },
     });
-    
+
     // Slider owlCarousel (Homepage Slider)
-    $('.slider-fade .owl-carousel').owlCarousel({
+    $(".slider-fade .owl-carousel").owlCarousel({
         items: 1,
         loop: true,
         dots: true,
         margin: 0,
-        autoplay: true,
+        autoplay: false,
         autoplayTimeout: 5000,
-        animateOut: 'fadeOut',
+        animateOut: "fadeOut",
         nav: true,
-        navText: ['<i class="ti-angle-left" aria-hidden="true"></i>', '<i class="ti-angle-right" aria-hidden="true"></i>'],
+        navText: [
+            '<i class="ti-angle-left" aria-hidden="true"></i>',
+            '<i class="ti-angle-right" aria-hidden="true"></i>',
+        ],
         responsiveClass: true,
         responsive: {
             0: {
-                dots: false
+                dots: false,
             },
             600: {
-                dots: false
+                dots: false,
             },
             1000: {
-                dots: true
-            }
-        }
+                dots: true,
+            },
+        },
     });
 
     // Clients owlCarousel *
-    $('.clients .owl-carousel').owlCarousel({
+    $(".clients .owl-carousel").owlCarousel({
         loop: true,
         margin: 30,
         mouseDrag: true,
-        autoplay: true,
+        autoplay: false,
         dots: false,
+        autoWidth: false,
         nav: false,
-        navText: ["<span class='lnr ti-angle-left'></span>","<span class='lnr ti-angle-right'></span>"],
+        navText: [
+            "<span class='lnr ti-angle-left'></span>",
+            "<span class='lnr ti-angle-right'></span>",
+        ],
         responsiveClass: true,
         responsive: {
             0: {
                 margin: 10,
-                items: 3
+                items: 3,
+                autoWidth: true,
             },
             600: {
-                items: 3
+                items: 3,
+                autoWidth: true,
             },
             1000: {
-                items: 3
-            }
+                items: 5,
+                autoWidth: false,
+            },
+        },
+    });
+    owl.on("changed.owl.carousel", function (event) {
+        var item = event.item.index - 2; // Position of the current item
+        $("span").removeClass("animated fadeInUp");
+        $("h5").removeClass("animated fadeInUp");
+        $(".btn").removeClass("animated fadeInUp");
+        $("h1").removeClass("animated fadeInUp");
+        $("p").removeClass("animated fadeInUp");
+        $(".butn-light").removeClass("animated fadeInUp");
+        $(".butn-dark").removeClass("animated fadeInUp");
+        $(".owl-item")
+            .not(".cloned")
+            .eq(item)
+            .find("span")
+            .addClass("animated fadeInUp");
+        $(".owl-item")
+            .not(".cloned")
+            .eq(item)
+            .find("h5")
+            .addClass("animated fadeInUp");
+        $(".owl-item")
+            .not(".cloned")
+            .eq(item)
+            .find("h1")
+            .addClass("animated fadeInUp");
+        $(".owl-item")
+            .not(".cloned")
+            .eq(item)
+            .find(".btn")
+            .addClass("animated fadeInUp");
+        $(".owl-item")
+            .not(".cloned")
+            .eq(item)
+            .find("p")
+            .addClass("animated fadeInUp");
+        $(".owl-item")
+            .not(".cloned")
+            .eq(item)
+            .find(".butn-light")
+            .addClass("animated fadeInUp");
+        $(".owl-item")
+            .not(".cloned")
+            .eq(item)
+            .find(".butn-dark")
+            .addClass("animated fadeInUp");
+    });
+
+    // MagnificPopup Gallery
+    $(".section6-image-popup").magnificPopup({
+        type: "image",
+        closeOnContentClick: !0,
+        mainClass: "mfp-fade",
+        gallery: {
+            enabled: !0,
+            navigateByImgClick: !0,
+            preload: [0, 1]
         }
     });
-    owl.on('changed.owl.carousel', function (event) {
-        var item = event.item.index - 2; // Position of the current item
-        $('span').removeClass('animated fadeInUp');
-        $('h4').removeClass('animated fadeInUp');
-        $('h1').removeClass('animated fadeInUp');
-        $('p').removeClass('animated fadeInUp');
-        $('.butn-light').removeClass('animated fadeInUp');
-        $('.butn-dark').removeClass('animated fadeInUp');
-        $('.owl-item').not('.cloned').eq(item).find('span').addClass('animated fadeInUp');
-        $('.owl-item').not('.cloned').eq(item).find('h4').addClass('animated fadeInUp');
-        $('.owl-item').not('.cloned').eq(item).find('h1').addClass('animated fadeInUp');
-        $('.owl-item').not('.cloned').eq(item).find('p').addClass('animated fadeInUp');
-        $('.owl-item').not('.cloned').eq(item).find('.butn-light').addClass('animated fadeInUp');
-        $('.owl-item').not('.cloned').eq(item).find('.butn-dark').addClass('animated fadeInUp');
+    $(".section9-image-popup").magnificPopup({
+        type: "image",
+        closeOnContentClick: !0,
+        mainClass: "mfp-fade",
+        gallery: {
+            enabled: !0,
+            navigateByImgClick: !0,
+            preload: [0, 1]
+        }
     });
+    $(".section10-image-popup").magnificPopup({
+        type: "image",
+        closeOnContentClick: !0,
+        mainClass: "mfp-fade",
+        gallery: {
+            enabled: !0,
+            navigateByImgClick: !0,
+            preload: [0, 1]
+        }
+    })
+  
+
 });
-
-
 
 // Preloader
 $("#preloader").fadeOut(700);
-	$(".preloader-bg").delay(700).fadeOut(700);
-	var wind = $(window);
-
-
-
-
+$(".preloader-bg").delay(700).fadeOut(700);
+var wind = $(window);
